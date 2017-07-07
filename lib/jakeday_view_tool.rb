206be65ed0662d0379679fc9a5f7ebfebf116e03
Jake Day Williams <jake@wildacademy.com>
2017-07-06 21:42:41 -0600

    Jake Day Tool initial commit

---
+++ lib/jakeday_view_tool.rb
@@ -0,0 +1,6 @@
+require "jakeday_view_tool/version"
+require "jakeday_view_tool/renderer"
+
+module JakedayViewTool
+  # Your code goes here...
+end
---
+++ lib/jakeday_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JakedayViewTool
+  VERSION = "0.1.0"
+end
